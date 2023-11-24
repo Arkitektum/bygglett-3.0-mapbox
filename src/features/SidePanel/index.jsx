aa413a37db84f0b364ac04fb7b39204c8dea4d2b
Ole <ole@arkitektum.no>
2023-11-24 15:38:06 +0100

    sidepanel

--- src/features/SidePanel/index.jsx
+++ src/features/SidePanel/index.jsx
@@ -1,14 +1,19 @@
 
+
 import styles from './SidePanel.module.scss'
-import { Reset, TurnLeft, TurnRight, ArrowBtn } from '../../assets/reset.jsx'
+
+
+
+
 
 function SidePanel() {
+    
    return (
-      <div className={styles.content}>
+      <div className={styles.content}>        
          <div className={styles.widthLabel}>
             <label htmlFor="reset">Nullstill visning</label>
             <button id="reset" className={styles.btn}>
-               <Reset />
+            <div className={styles.reset}></div>
             </button>
          </div>
 
@@ -16,14 +21,14 @@ function SidePanel() {
             <div>
                <label className={styles.hidden} htmlFor="left">Nullstill visning</label>
                <button id="left" className={styles.btn}>
-                  <ArrowBtn />
+               <div className={styles.arrow}></div>
                </button>
             </div>
             <div>
                <label className={styles.hidden} htmlFor="up">Nullstill visning</label>
                <button id="up" className={styles.btn}>
                   <div className={styles.rotateOnce}>
-                     <ArrowBtn />
+                    <div className={styles.arrow}></div>
                   </div>
                </button>
             </div>
@@ -31,7 +36,7 @@ function SidePanel() {
                <label className={styles.hidden} htmlFor="right">Nullstill visning</label>
                <button id="right" className={styles.btn}>
                   <div className={styles.rotateTwice}>
-                     <ArrowBtn />
+                  <div className={styles.arrow}></div>
                   </div>
                </button>
             </div>
@@ -39,27 +44,39 @@ function SidePanel() {
                <label className={styles.hidden} htmlFor="down">Nullstill visning</label>
                <button id="down" className={styles.btn}>
                   <div className={styles.rotateThird}>
-                     <ArrowBtn />
+                  <div className={styles.arrow}></div>
                   </div>
                </button>
             </div>
             <div className={styles.house}>
-               <img src='/src/assets/gfx/icon-house-3d.svg' />
+                <div className={styles.house3d}></div>
             </div>
+            <div className={styles.label}>Flytte bygningen</div>
          </div>
 
          <div className={styles.rotations}>
             <button className={styles.left}></button>
-            <div className={styles.house3d}></div>
+                <div className={styles.house3d}></div>
             <button className={styles.right}></button>
+            <div>Rotere bygningen</div>
          </div>
 
          <div className={styles.elevations}>
             <button className={styles.up}></button>
-            <div className={styles.house3d}></div>
+                <div className={styles.house3d}></div>
             <button className={styles.down}></button>
+            <div>Heve/Senke bygningen</div>
          </div>
+         <div className={styles.spm}>
+            <div className={styles.spmIcon}></div>
+            
+            
+         </div>
+
+        
+
       </div>
+      
    );
 }
 
