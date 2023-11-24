447347813791ad0e323a0694126c1b89b8aab28d
Ole <ole@arkitektum.no>
2023-11-24 12:31:01 +0100

    Auto stash before rebase of "origin/main"

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import MapView from 'features/MapView';
 import styles from './App.module.scss'
 import { useSelector } from 'react-redux';
 import { useEffect } from 'react';
+import logo from 'assets/dibk.svg';
+import SidePanel from 'features/SidePanel'
 
 function App() {
    const location = [6.014302890886796, 60.265660111660935];
@@ -21,15 +23,22 @@ function App() {
 
    return (
       <div className={styles.app}>
+      <div className={styles.banner}><div><img src={logo} width="100px" /></div> </div>
+         <div className={styles.textbox}>
+         <p>Demonstrator for plassering av byggninger og påbygg og hva som er mulig innenfor området i kartet.</p>
+            <p>For å flytte på bygningen: Klikk på bygningen så rammen blir grønn. Beveg huset med muspeker ved å holde shift-tasten inne. Bygningen kan roteres med å holde alt-tasten inne. Skal du bevege bygning vertikalt så hold ctrl-tasten inne</p>
+         </div>
+         <div className={styles.container}> 
          <MapProvider location={location} altitude={altitude} building={building}>
-            <div className={styles.sidePanel}>
-               <h3>Sidepanel</h3>
-            </div>
 
+            <div className={styles.sidePanel}>
+               <SidePanel />               
+            </div>
             <div className={styles.mapView}>
                <MapView />
             </div>            
          </MapProvider>
+         </div>
       </div>
    );
 }
