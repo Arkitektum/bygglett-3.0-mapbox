8150760edf4b4d8c192674fcb26aa0eda00cefdd
Matias <matias@arkitektum.no>
2023-11-29 10:07:54 +0100

    measure distance to closest feature

--- src/utils/map/building.js
+++ src/utils/map/building.js
@@ -1,7 +1,18 @@
 import booleanIntersects from '@turf/boolean-intersects';
 import { getObjectArea } from 'utils/helpers';
+import buffer from '@turf/buffer';
+import { degreesToRadians, feature, radiansToLength } from '@turf/helpers';
 import { setIntersection } from 'store/slices/mapSlice';
 import store from 'store';
+import GeoJSONReader from 'jsts/org/locationtech/jts/io/GeoJSONReader';
+import GeoJSONWriter from 'jsts/org/locationtech/jts/io/GeoJSONWriter';
+import DistanceOp from 'jsts/org/locationtech/jts/operation/distance/DistanceOp';
+
+
+// TODO filter geoJson før import
+
+
+
 
 export function createBuilding(map, location, altitude, building) {
    map.addLayer({
@@ -9,7 +20,7 @@ export function createBuilding(map, location, altitude, building) {
       type: 'custom',
       renderingMode: '3d',
       onAdd: () => {
-         const scale = .5;
+         const scale = 0.6;
 
          const options = {
             obj: `/${building}.glb`,
@@ -24,6 +35,7 @@ export function createBuilding(map, location, altitude, building) {
             model.setCoords([location[0], location[1], altitude]);
 
             addObjectLayer(map, model);
+            addObjectBufferLayer(map, model)
 
             model.addEventListener('ObjectDragged', event => {
                handleObjectDragged(event.target, map);
@@ -41,11 +53,38 @@ export function createBuilding(map, location, altitude, building) {
 
 function handleObjectDragged(object, map) {
    const area = getObjectArea(object);
-   const source = map.getSource('object-area');
+   map.getSource('object-area').setData(area);
 
-   source.setData(area);
+   const areaOfInterest = buffer(area, 50, {
+      units: 'meters'
+   })
+   map.getSource('object-bufferArea').setData(areaOfInterest);
 
-   const features = map.querySourceFeatures('naturtyper-utvalgte', { sourceLayer: 'naturtyper-utvalgte' });
+
+   const reader = new GeoJSONReader();
+   const areaGeometry = reader.read(area.geometry);
+
+   // Filter polygons based on intersection or inclusion
+   const sourceFeatures = map.querySourceFeatures('naturtyper-utvalgte');
+   const features = sourceFeatures.filter(feature => booleanIntersects(feature.geometry, areaOfInterest));
+
+   if (features != null) {
+
+      const distanceArray = features.map((feature) => {
+
+         const reader = new GeoJSONReader();
+         const featureGeometry = reader.read(feature.geometry);
+
+         const distanceInDegrees = new DistanceOp(featureGeometry, areaGeometry).distance()
+         const distanceInRadians = degreesToRadians(distanceInDegrees)
+         const distanceInMeter = radiansToLength(distanceInRadians, 'meters')
+         return distanceInMeter
+
+      });
+      const minDistanceInMeter = Math.min(...distanceArray);
+      console.log("korteste avstand i mts : " + minDistanceInMeter)
+   }
+
    const intersecting = features.find(feature => booleanIntersects(feature.geometry, area));
    let color = 'green';
 
@@ -73,6 +112,27 @@ function addObjectLayer(map, model) {
       paint: {
          'fill-color': 'green',
          'fill-opacity': 0.4
+      }
+   });
+}
+function addObjectBufferLayer(map, model) {
+   const area = getObjectArea(model);
+   const areaBuffer = buffer(area, 50, {
+      units: 'meters'
+   })
+
+   map.addSource('object-bufferArea', {
+      type: 'geojson',
+      data: areaBuffer
+   });
+
+   map.addLayer({
+      id: 'object-byfferArea',
+      type: 'fill',
+      source: 'object-bufferArea',
+      paint: {
+         'fill-color': 'blue',
+         'fill-opacity': 0.2
       }
    });
 }
